35c58f87de165d177dbf910057ad35dad12db206
Mikita Sobal <soboln2006@gmail.com>
2025-04-13 18:11:42 +0300

    create menu, nav

--- code/__recovery/MainUnit.pas
+++ code/__recovery/MainUnit.pas
@@ -17,6 +17,14 @@ type
     btnNavAnalytics: TButton;
     pnMapWrap: TPanel;
     imgMap: TImage;
+    pnCreateObject: TPanel;
+    btnCreateShop: TButton;
+    btnCreateWarehouse: TButton;
+    btnCreateCancel: TButton;
+    spMapPoint: TShape;
+    procedure imgMapMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure btnCreateCancelClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,5 +37,25 @@ var
 implementation
 
 {$R *.dfm}
+
+
+procedure TfrMainForm.btnCreateCancelClick(Sender: TObject);
+begin
+  pnCreateObject.Visible := false;
+  spMapPoint.Visible := false;
+end;
+
+procedure TfrMainForm.imgMapMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  pnCreateObject.top := Y-pnCreateObject.Height;
+  pnCreateObject.left := X;
+  pnCreateObject.Visible := true;
+
+  spMapPoint.top := Y - spMapPoint.Height shr 1;
+  spMapPoint.left := X - spMapPoint.Width shr 1;
+  spMapPoint.Visible := true;
+
+end;
 
 end.
